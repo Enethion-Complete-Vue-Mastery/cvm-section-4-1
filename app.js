358b5f01883a09ecbf7130023015466451c97fb6
Piotr <straszak.piotr@gmail.com>
2022-02-28 11:04:38 +0100

    Yay... Section complete

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 let vm = Vue.createApp({
-  data() {
+  
+})
+
+vm.component('hello', {
+  data(){
     return {
-      message: "Hello world!"
+      message: 'Hello world'
     }
-  }
-}).mount('#app')
+  },
+  template: `<h1>{{message}}</h1>`
+})
+
+vm.mount('#app')
+
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!"
+//     }
+//   },
+//   template: `{{message}}`,
+//   render() {
+//     return Vue.h(
+//       'h1',
+//       this.message
+//     )
+//   }
+// }).mount("#app2")
